942f6862ffad4eb31db4c76cf481f60cfe5174d6
Tarun gupta <135594443+tGupta24@users.noreply.github.com>
2026-06-20 23:48:15 +0530

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    bool valid(unordered_map<char,int>&mp){
+        for(auto &i:mp){
+            if(i.second>0) return false;
+        }
+        return true;
+    }
+    string minWindow(string s, string t) {
+        int n = s.size();
+        unordered_map<char,int>mp;
+        int len= INT_MAX;
+        int start = 0;
+        for(auto i:t){
+            mp[i]++;
+        }
+        int i=0;
+        int j=0;
+
+        while(j<n){
+            if(mp.count(s[j])){
+                mp[s[j]]--;
+            }
+
+            while(valid(mp)){
+                if(len>j-i+1){
+                len = j-i+1;
+                start = i;
+                }
+                    
+                if(mp.count(s[i])){
+                    mp[s[i]]++;
+                }
+                i++;
+            }
+            j++;
+        }
+        if(len == INT_MAX) return "";
+        return s.substr(start,len);
+    }
+};
+
+
+// x1
+// y1
+// z1
+
+
+// OUZODYXAZV
+//       j
+//    i
+// while(valid){
+//     shrink
+// }
